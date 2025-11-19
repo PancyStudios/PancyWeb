95f13a2d9793190fd44c2c77c6bb63d2b314269e
ImTurbis <73681786+ImTurbis@users.noreply.github.com>
2025-11-19 08:02:25 -0600

    Update Next 16.0.3

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
 	// --- 1. PROTECCIÓN DE DOMINIO ---
 	const hostHeader = request.headers.get('host') || '';
 	const hostname = hostHeader.split(':')[0];
