22cb19a79f4cb89c8ecce512c556dd4f2b758eae
ImTurbis <73681786+ImTurbis@users.noreply.github.com>
2025-11-19 07:40:19 -0600

    Minior Fix

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import Image from "next/image";
 
 export default function UserButton() {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
